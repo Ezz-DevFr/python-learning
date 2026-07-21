fbc4e71def06cec9a40c90270b74aab79337e4e4
Ezz-DevFr <ezzmainacc@gmail.com>
2026-07-22 01:55:34 +0300

    more

---
+++ lesson_009_lists.py
@@ -0,0 +1,7 @@
+games = ["dmc", "RE4", "RL", "GTG", "FN"]
+print(games[0])
+print(games[4])
+games[0] = "MGS"
+print(games)
+games.append("GOW")
+print(games)
---
+++ lesson_010_dictionary.py
@@ -0,0 +1,11 @@
+car = {
+    "brand": "SKODA",
+    "model": "octavia",
+    "year": 2019
+}
+print(car)
+print(car["brand"])
+car["year"] = "2020"
+print(car["year"])
+car["colour"] = "yellow"
+print(car)
